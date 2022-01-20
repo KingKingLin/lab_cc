e0b91b2efce487deea0b3d13a06009e09b058c49
KingKingLin <827543964@qq.com>
2022-01-20 23:14:52 +0800

    day3 解决刷新登录信息消失的问题

--- src/main/java/com/cc/lab_teach/exception/BusinessExceptionCode.java
+++ src/main/java/com/cc/lab_teach/exception/BusinessExceptionCode.java
@@ -2,8 +2,8 @@ package com.cc.lab_teach.exception;
 
 public enum BusinessExceptionCode {
 
-    Not_EXITS("此学号, 请联系相关教师"),
-    LOGIN_STUDENT_ERROR("学号不存在或密码错误"),
+    Not_EXITS("此学号不存在, 请联系相关教师"),
+    LOGIN_STUDENT_ERROR("密码错误"),
     LOGIN_TEACHER_ERROR("教工号不存在或密码错误"),
     ;
 
--- web/src/store/user.js
+++ web/src/store/user.js
@@ -2,21 +2,24 @@ export default {
     // 开启命名空间
     namespaced: true,
     state: {
-        token: '',
+        token: SessionStorage.get('user') || '',
         // user: {id: xx, name: xx}
-        user: {},
+        user: SessionStorage.get('user') || {},
         // 0 表示学生 1 表示教师
-        type: 0
+        type: SessionStorage.get('user') || 0
     },
     mutations: {
         setToken(state, token) {
             state.token = token
+            SessionStorage.set('token', token)
         },
         setUser(state, user) {
             state.user = user
+            SessionStorage.set('user', user)
         },
         setType(state, type) {
             state.type = type
+            SessionStorage.set('type', type)
         }
     }
 }
